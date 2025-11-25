27e58691df64ae83e6334392fafbbcbc4acaf035
Johan Mesa <Johanotr@gmail.com>
2025-11-25 12:04:57 -0500

    feat: add header content

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,43 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styles from './app.module.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <nav className={styles.nav}>
+        <div className={styles.navContent}>
+          <a href="#">Johan M. Mesa</a>
+          <a href="#">Resume</a>
+        </div>
+      </nav>
+      <header className={styles.headerContainer}>
+          <div className={styles.headerContent}>
+            <div className={styles.imageContainer}>
+              <img className={styles.profilePicture} src="/profile_Picture.jpg" alt="Picture of Johan Mesa" />
+            </div>
+            <div className={styles.summary}>
+              <div className={styles.externalLinks}>
+                  <a href="https://github.com/GravityCodes" target="_blank"><img src="/github-logo-white.svg" alt="github" /></a>
+                  <a href="https://www.linkedin.com/in/johan-mesa/"><img src="/linkedin-logo.svg" alt="Linkedin" /></a>
+              </div>
+              <p className={styles.paragraph}>
+                Fullstack developer based in Boston, MA with experience building complete web applications from UI to database.
+                Strong problem solving abilities and a focus on writing clean, maintainable, and reliable code. Committed to
+                continuously improving my development skills through real world projects.
+              </p>
+            </div>
+          </div>
+      </header>
+      <main className={styles.mainContainer}>
+        <div className={styles.recentProjects}>
+          <h2 className={styles.sectionTitle}>RECENT PROJECTS</h2>
+
+        </div>
+
+      </main>
+    </div>
   )
 }
 
